0081a360420802e9097883e9fd0c905b9d82bbcf
GitWilder <43479032+Wilder101@users.noreply.github.com>
2018-10-30 17:52:22 -0700

    Initial logic ideal

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,35 @@
 // TriviaGame by WBM, 10/28/18, app.js file
+
+$(document).ready(function() {
+
+    // Variables
+    questionNumber = 0;             // Tracker for which question game is on
+
+
+    // Array of trivia questions
+    var questionDB = [
+        {question: "What time is it?",  
+        answers: ["07:00", "12:00", "18:30", "23:59"],
+        correctAnswer: 1} 
+
+        // {name: "yellow" , randomHiddenValue: 0}
+    ];
+
+
+    function showQuestion(whichQuestion) {
+
+        // Show a question and possible answers
+        $("#the-question").text(questionDB[whichQuestion].question);
+        
+        $("#btn-top").text(questionDB[whichQuestion].answers[0]);
+        $("#btn-middle-top").text(questionDB[whichQuestion].answers[1]);
+        $("#btn-middle-bottom").text(questionDB[whichQuestion].answers[2]);
+        $("#btn-bottom").text(questionDB[whichQuestion].answers[3]);
+    };
+
+    // On page load
+    showQuestion(questionNumber);
+
+
+
+}); // End document.ready
