a7ce21deaa1b838989f32e0de03a8d5d99a51af2
GitWilder <43479032+Wilder101@users.noreply.github.com>
2018-11-03 22:18:34 -0700

    progress

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,7 +3,7 @@
 $(document).ready(function() {
 
     // Variables
-    let questionNumber = 0;             // Tracker for which question game is on
+    let questionNumber = 5;             // Tracker for which question game is on
     let numberOfCorrectAnswers = 0;     // Counter for player's number of correct answers
     let numberOfWrongAnswers = 0;       // Counter for player's number of wrong
     let timeRemaining = 30;             // Count down time remaining in question selection
@@ -72,6 +72,9 @@ $(document).ready(function() {
             // Check for winning round
             if (whichAnswerSelected == questionDB[questionNumber].correctAnswer) {
 
+                // Update score tracking
+                numberOfCorrectAnswers++;
+
                 // Clear all content and show time remaining
                 $("#content").empty();
                 showTimeRemaining();
@@ -81,21 +84,23 @@ $(document).ready(function() {
                 correctDisplay.text("Correct!");
                 $("#content").append(correctDisplay);
                 // INCLUDE IMAGE LATER
-
             }
             // Otherwise guess was a miss
             else {
+
+                // Update score tracking
+                numberOfWrongAnswers++;
 
                 // Clear all content and show time remaining
                 $("#content").empty();
                 showTimeRemaining();
 
                 // Show incorrect
-                var incorrectDisplay = $("<h2>").addClass("p-1");
+                let incorrectDisplay = $("<h2>").addClass("p-1");
                 incorrectDisplay.html("Incorrect.");
 
                 // Add the correct answer
-                var theAnswerWas = $("<h3>").addClass("p-1");
+                let theAnswerWas = $("<h3>").addClass("p-1");
                 let correctIndex = questionDB[questionNumber].correctAnswer;
                 theAnswerWas.html("The correct answer was: <br>" + questionDB[questionNumber].answers[correctIndex]);
                 incorrectDisplay.append(theAnswerWas);
@@ -107,21 +112,68 @@ $(document).ready(function() {
             // Update game state to next question or end game
             updateGameState();
 
-            // $("#content").append("Answer button selected");
-
-            // TESTING
-            // $("#content").empty();
-
         }); // End handle answer selection on click event
     } // End of showPossibleAnswers function
 
+    // Update game state to advance question or end game 
     function updateGameState() {
+
         // Update game state to next question
         questionNumber++;
 
         // Check for end of game
         if ( questionNumber >= questionDB.length - 1) {
-            // END OF GAME; SHOW RESULTS
+            
+            // End of game;show results
+            // On the final screen, show the number of correct answers, incorrect answers, and an option to restart the game (without reloading the page).
+            $("#content").empty();                  // Clear content
+
+            // Show number of correct and incorrect answers
+            let showScoreResults = $("<h2>").addClass("p-1");
+            showScoreResults.html("Thank you for playing. <br><br>");
+            showScoreResults.append("Correct answers: " + numberOfCorrectAnswers + "<br>");
+            showScoreResults.append("Incorrect answers: " + numberOfWrongAnswers);
+            $("#content").append(showScoreResults);
+            
+            // Restart game and button
+            let restartButton = $("<button>");
+            restartButton.addClass("btn btn-lg btn-light");
+            restartButton.attr("type", "button");
+            restartButton.attr("id", "restart-button");
+            restartButton.text("Restart");
+            $("#content").append("<br><br><br>");
+            $("#content").append(restartButton);
+
+            // Restart the game event listener
+            $("#restart-button").on("click", function() {
+
+                // Reset game variables
+                questionNumber = 0;             // Tracker for which question game is on
+                numberOfCorrectAnswers = 0;     // Counter for player's number of correct answers
+                numberOfWrongAnswers = 0;       // Counter for player's number of wrong
+                timeRemaining = 30;             // Count down time remaining in question selection
+
+                // Clear all content
+                $("#content").empty();
+
+                // Show time remaining
+                showTimeRemaining();
+
+                // Display first question
+                showQuestion(questionNumber);
+
+                // Show answer set
+                showPossibleAnswers(questionNumber);
+
+            }); // End start start button on click event
+
+        }
+        // Game is to continue -- BE SURE TO INCLUDE A DELAY TIMER IF GAME IS TO CONTINUE, FIRST 
+        else {
+            $("#content").empty();                  // Clear content
+            showTimeRemaining();                    // Show time remaining
+            showQuestion(questionNumber);           // Display next question
+            showPossibleAnswers(questionNumber);    // Show answer set
         }
     }; // End of updateGameState function
 
@@ -142,12 +194,6 @@ $(document).ready(function() {
 
         // Show time remaining
         showTimeRemaining();
-        // var timeDisplay = $("<h2>");
-        // timeDisplay.addClass("p-1");
-        // timeDisplay.html("Time Remaining: <span id='time-remaining'></span> Seconds");
-        // $("#content").append(timeDisplay);
-        // $("#content").append("<hr></hr>");
-        // $("#time-remaining").text(timeRemaining);
 
         // Display first question
         showQuestion(questionNumber);
